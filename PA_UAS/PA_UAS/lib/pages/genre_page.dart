923000be41b71555c966e0df22e64187bca4dd94
Kuuhaku456 <manikfilip03@gmail.com>
2023-11-29 11:17:52 +0800

    Add Genre Page

---
+++ PA_UAS/PA_UAS/lib/pages/genre_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyGenrePage extends StatefulWidget {
+  const MyGenrePage({super.key});
+
+  @override
+  State<MyGenrePage> createState() => _MyGenrePageState();
+}
+
+class _MyGenrePageState extends State<MyGenrePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF374259),
+    );
+  }
+}
